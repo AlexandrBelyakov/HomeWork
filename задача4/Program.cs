260a71ba91a98059dbbfb33913e2e36e2e5a7757
AlexandrBelyakov <belya0309@mail.ru>
2023-03-27 00:48:11 +0300

    Задача 4

--- задача4/Program.cs
+++ задача4/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine(" Hello, World4!");
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int startN = 1;
+
+while(startN <= n)
+    {
+        if(startN % 2 ==0)
+        Console.Write(startN + ", ");
+        startN ++;
+}
